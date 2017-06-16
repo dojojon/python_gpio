d12c9706a7aebc5d11832d3f5d593b27e7155d2b
Jon Hearn <hearnjon@DUB8476OSX1010.local>
2017-06-16 21:33:56 +0100

    shutdown pi script added

---
+++ shutdownpis.sh
@@ -0,0 +1,12 @@
+echo 101
+sshpass -p 'raspberry' ssh pi@192.168.0.101 "sudo shutdown -h now"
+echo 102
+sshpass -p 'raspberry' ssh pi@192.168.0.102 "sudo shutdown -h now"
+echo 103
+sshpass -p 'raspberry' ssh pi@192.168.0.103 "sudo shutdown -h now"
+echo 104
+sshpass -p 'raspberry' ssh pi@192.168.0.104 "sudo shutdown -h now"
+echo 105
+sshpass -p 'raspberry' ssh pi@192.168.0.105 "sudo shutdown -h now"
+echo 110
+sshpass -p 'raspberry' ssh pi@192.168.0.110 "sudo shutdown -h now"
